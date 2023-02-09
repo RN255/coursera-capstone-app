7e089a808fa1acd3ee85b215ee3e211dc5514e46
Robert Nickson <106345947+RN255@users.noreply.github.com>
2023-02-09 16:38:27 +0000

    nav and header progressing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,24 @@
 import React from "react";
+import restaurantFood from "../assets/restaurantfood.jpg";
 
 export default function Header() {
   return (
     <>
       <header>
-        Header
+        <div id="headerContainer">
+          <div>
+            <h1>Little Lemon</h1>
+            <h2>Chicago</h2>
+            <p>
+              We are a family owned Mediterranean resturany, focused on
+              traditional recipes served with a modern twist.
+            </p>
+            <button>Reserve a Table</button>
+          </div>
+          <div id="headerImgContainer">
+            <img src={restaurantFood} alt="some food"></img>
+          </div>
+        </div>
       </header>
     </>
   );
